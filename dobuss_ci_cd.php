10fac77784696be3d5f8a39040074c72bdf6e9f6
Antonio <aolmo@dobuss.es>
2019-10-23 12:17:18 +0200

    Subida de Plugin

--- dobuss_ci_cd.php
+++ dobuss_ci_cd.php
@@ -3,7 +3,7 @@
 Plugin Name: Dobuss CI/CD
 Plugin URI: www.dobuss.es
 Description: Plugin de Automatizacion dobuss
-Version: 2.3
+Version: 2.4
 Author: Antonio
 Author URI:www.dobuss.es
 License: GPL
