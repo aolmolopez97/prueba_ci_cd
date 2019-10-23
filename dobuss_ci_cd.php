b8cc4c4f494d44c42277b315002a637050510554
Antonio <aolmo@dobuss.es>
2019-10-23 12:12:08 +0200

    Subida de Plugin

--- dobuss_ci_cd.php
+++ dobuss_ci_cd.php
@@ -2,8 +2,8 @@
 /*
 Plugin Name: Dobuss CI/CD
 Plugin URI: www.dobuss.es
-Description: Plugin de Google Reviews desarrollado por dobuss que inserta reseñas de tu ficha de google en tu web
-Version: 1.0
+Description: Plugin de CI/CD
+Version: 2.0
 Author: Antonio
 Author URI:www.dobuss.es
 License: GPL
